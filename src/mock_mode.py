d8600ee30a6904bf3b0d5b4ad0160d853cf73125
York Jong <yukuan.jiang@gmail.com>
2023-05-11 23:59:16 +0800

    Update mock_mode.py

--- src/mock_mode.py
+++ src/mock_mode.py
@@ -10,8 +10,8 @@ import os
 def init_environ_variables():
     print('init_environ_variables')
 
-    os.environ['SERVICE_ACCOUNT_INFO'] =
-        'SERVICE_ACCOUNT_INFO_TO_ACCESS_GOOGLE_DRIVE'
+    os.environ[
+        'SERVICE_ACCOUNT_INFO'] = 'SERVICE_ACCOUNT_INFO_TO_ACCESS_GOOGLE_DRIVE'
 
     os.environ['FOLDER_ID'] = 'ID_OF_THE_FOLDER_ON_THE_GOOGLE_DRIVE'
 
